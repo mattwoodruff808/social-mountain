e47691650fcc0738b39107d8e259671f938d52e3
mattwoodruff808 <mjwoodruff93@gmail.com>
2020-12-28 15:22:42 -0700

    Finished step 5, wanted to save work

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Post from './Post/Post';
 
 class App extends Component {
   constructor() {
@@ -19,15 +21,21 @@ class App extends Component {
   }
   
   componentDidMount() {
-
+    axios
+      .get('https://practiceapi.devmountain.com/api/posts')
+      .then(res => this.setState({posts: res.data}))
   }
 
-  updatePost() {
-  
+  updatePost(id, text) {
+    axios
+      .put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, {text})
+      .then(res => this.setState({posts: res.data}))
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    axios
+      .delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`)
+      .then(res => this.setState({posts: res.data}))
   }
 
   createPost() {
@@ -44,7 +52,14 @@ class App extends Component {
         <section className="App__content">
 
           <Compose />
-          
+          {posts.map((post) => (
+            <Post key={post.id} 
+                  text={post.text} 
+                  date={post.date}
+                  updatePostFn={this.updatePost} 
+                  id={post.id}
+                  deletePostFn={this.deletePost}/>
+          ))}
         </section>
       </div>
     );
